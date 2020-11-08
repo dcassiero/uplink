ae910c063b33a372659b943648b1e9b0312223de
Daniel Cassiero <daniel.cassiero@gmail.com>
2020-11-07 23:51:36 -0500

    adding lame dos script

---
+++ tools/builder.bat
@@ -0,0 +1,36 @@
+docker build --build-arg game=alien --build-arg exe=ALIEN.EXE -t dcassiero/em_dosbox-alien:uplink-v1.0 .
+docker push dcassiero/em_dosbox-alien:uplink-v1.0
+docker build --build-arg game=brain --build-arg exe=BRAIN.EXE -t dcassiero/em_dosbox-brain:uplink-v1.0 .
+docker push dcassiero/em_dosbox-brain:uplink-v1.0
+docker build --build-arg game=carmen --build-arg exe=CARMEN.EXE -t dcassiero/em_dosbox-carmen:uplink-v1.0 .
+docker push dcassiero/em_dosbox-carmen:uplink-v1.0
+docker build --build-arg game=cvania --build-arg exe=CEGA.EXE -t dcassiero/em_dosbox-cvania:uplink-v1.0 .
+docker push dcassiero/em_dosbox-cvania:uplink-v1.0
+docker build --build-arg game=dinoadv --build-arg exe=KAV.EXE -t dcassiero/em_dosbox-dinoadv:uplink-v1.0 .
+docker push dcassiero/em_dosbox-dinoadv:uplink-v1.0
+docker build --build-arg game=doom --build-arg exe=DOOM.EXE -t dcassiero/em_dosbox-doom:uplink-v1.0 .
+docker push dcassiero/em_dosbox-doom:uplink-v1.0
+docker build --build-arg game=dune --build-arg exe=DUNE.BAT -t dcassiero/em_dosbox-dune:uplink-v1.0 .
+docker push dcassiero/em_dosbox-dune:uplink-v1.0
+docker build --build-arg game=gadget --build-arg exe=gadget.exe -t dcassiero/em_dosbox-gadget:uplink-v1.0 .
+docker push dcassiero/em_dosbox-gadget:uplink-v1.0
+docker build --build-arg game=isaac --build-arg exe=KAV.EXE -t dcassiero/em_dosbox-isaac:uplink-v1.0 .
+docker push dcassiero/em_dosbox-isaac:uplink-v1.0
+docker build --build-arg game=lodetlr --build-arg exe=LR.BAT -t dcassiero/em_dosbox-lodetlr:uplink-v1.0 .
+docker push dcassiero/em_dosbox-lodetlr:uplink-v1.0
+docker build --build-arg game=mlbb --build-arg exe=Mlbb.com -t dcassiero/em_dosbox-mlbb:uplink-v1.0 .
+docker push dcassiero/em_dosbox-mlbb:uplink-v1.0
+docker build --build-arg game=nba --build-arg exe=nba.exe -t dcassiero/em_dosbox-nba:uplink-v1.0 .
+docker push dcassiero/em_dosbox-nba:uplink-v1.0
+docker build --build-arg game=oregondx --build-arg exe=OREGON.EXE -t dcassiero/em_dosbox-oregondx:uplink-v1.0 .
+docker push dcassiero/em_dosbox-oregondx:uplink-v1.0
+docker build --build-arg game=prince --build-arg exe=PRINCE.EXE -t dcassiero/em_dosbox-prince:uplink-v1.0 .
+docker push dcassiero/em_dosbox-prince:uplink-v1.0
+docker build --build-arg game=sgate --build-arg exe=SGATE.EXE -t dcassiero/em_dosbox-sgate:uplink-v1.0 .
+docker push dcassiero/em_dosbox-sgate:uplink-v1.0
+docker build --build-arg game=sq1 --build-arg exe=SQ.COM -t dcassiero/em_dosbox-sq1:uplink-v1.0 .
+docker push dcassiero/em_dosbox-sq1:uplink-v1.0
+docker build --build-arg game=ultima6 --build-arg exe=ULTIMA6.EXE -t dcassiero/em_dosbox-ultima6:uplink-v1.0 .
+docker push dcassiero/em_dosbox-ultima6:uplink-v1.0
+docker build --build-arg game=ys --build-arg exe=ys.exe -t dcassiero/em_dosbox-ys:uplink-v1.0 .
+docker push dcassiero/em_dosbox-ys:uplink-v1.0
